b63a1548a359dc8e0caddf11c7c0c3865ef49277
Stephen Byrne <stephen.byrne@measuresforjustice.org>
2019-03-07 10:24:04 -0500

    better SAM explanation

--- intro/src/main/java/io/mfj/kotlinnight/intro/SAM.kt
+++ intro/src/main/java/io/mfj/kotlinnight/intro/SAM.kt
@@ -3,23 +3,35 @@ package io.mfj.kotlinnight.intro
 import java.io.File
 import kotlin.concurrent.thread
 
+// Java SAM (Single Abstract Method) interfaces can be called with a lambda.
 object SAM {
 
 	@JvmStatic
 	fun main( args:Array<String> ) {
 
+		// Thread's constructor takes a Runnable, which has a single abstract method, "run"
+
+		// Here is how we would do that with an anonymous object:
+		Thread(
+				object:Runnable {
+					override fun run() {
+						println( "Hello world [${Thread.currentThread().name}]" )
+					}
+				}
+		).start()
+
+		// It is much nicer if we just pass a lambda:
 		Thread {
 			println( "Hello world [${Thread.currentThread().name}]" )
 		}.start()
 
-		thread {
-			println( "Hello World [${Thread.currentThread().name}]" )
-		}
-
+		// Tangent: this is so common, that Kotlin has an extension function
+		// to create threads with lambda, and gives other useful arguments:
 		thread(start = true, name = "hello-thread",isDaemon = false) {
 			println( "Hello World ${Thread.currentThread().name}]" )
 		}
 
+		// File.listFiles(FileFilter) is another common SAM case:
 		/*
 		File(System.getProperty("user.home"))
 				.listFiles { file:File ->
@@ -32,11 +44,11 @@ object SAM {
 
 	}
 
-	// You can only do SAM conversions with Java interfaces.
-	// If you want to use a lambda when calling a Kotlin function, use first class functions.
-	// Type aliases can make this look nicer
-
 }
+
+// You can only do SAM conversions with Java interfaces.
+// If you want to use a lambda when calling a Kotlin function, use first class functions.
+// Type aliases can make this look nicer
 
 typealias Handler = (Int)->Int
 
