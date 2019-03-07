3c70de9414e610006dc603d796ebb80c1a9d3e6e
Stephen Byrne <stephen.byrne@measuresforjustice.org>
2019-03-07 10:17:33 -0500

    passing functions

--- intro/src/main/java/io/mfj/kotlinnight/intro/Functional.kt
+++ intro/src/main/java/io/mfj/kotlinnight/intro/Functional.kt
@@ -2,16 +2,11 @@ package io.mfj.kotlinnight.intro
 
 import java.util.concurrent.TimeUnit
 
+// In Kotlin, functions are first-class!
+
+// Anything you can do with a value, you can do with a function.
+
 object Functional {
-
-	@JvmStatic
-	fun main( args:Array<String> ) {
-
-		time(100) {
-			println("Hello World" )
-		}
-
-	}
 
 	// body takes zero arguments, and returns Unit
 	fun time( times:Int, body:()->Unit ) {
@@ -23,12 +18,46 @@ object Functional {
 		println( "Doing a thing ${times} times took ${TimeUnit.NANOSECONDS.toMillis(t1-t0)} ms" )
 	}
 
+	// let's run it
+	@JvmStatic
+	fun main( args:Array<String> ) {
+
+		time(100) {
+			println("Hello World" )
+		}
+
+		// you can store functions as variables and pass them.
+		val body = {
+			println("Hello World" )
+		}
+		time(1,body)
+
+	}
+
 	// let's make the body take a parameter and return something
 
 }
 
+// You can have a function as a class member
+class HasAFunction( val handler:(String,String)->String ) {
+
+	fun doStuff( a:String, b:String ): String {
+		return handler(a,b)
+	}
+
+	companion object {
+		@JvmStatic
+		fun main( args:Array<String> ) {
+			val result = HasAFunction( { a, b -> "${a} ${b}" } )
+					.doStuff( "hello", "world" )
+			println( result )
+		}
+	}
+
+}
+
 // You can define functions in any scope
-object InnerFUnction{
+object InnerFunction {
 
 	@JvmStatic
 	fun main( args:Array<String> ) {
